d108594523ad0357aed03a75c483c1305689ea65
kapsona777 <lasha.kapanadze.1@btu.edu.ge>
2022-11-17 17:03:41 +0400

    update

--- routes/posts.js
+++ routes/posts.js
@@ -15,12 +15,12 @@ router.get('/', verify, async (req,res) => {
     }
 });
 
-router.get('/specific', (req,res) => {
+router.get('/specific',verify, (req,res) => {
     res.send("Specific post");
 });
 
 // SUBMITS A POST
-router.post('/', (req,res) => {
+router.post('/', verify,(req,res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
@@ -36,7 +36,7 @@ router.post('/', (req,res) => {
 })
 
 // SPECIFIC POST
-router.get('/:postId', async (req,res) => {
+router.get('/:postId',verify, async (req,res) => {
     try{
         const post = await Post.findById(req.params.postId);
         res.json(post);
@@ -47,7 +47,7 @@ router.get('/:postId', async (req,res) => {
 });
 
 // DELETE POST
-router.delete('/:postId', async (req,res) => {
+router.delete('/:postId', verify, async (req,res) => {
     try{
         const removedPost = await Post.remove({_id: req.params.postId});
         res.json(removedPost);
@@ -57,7 +57,7 @@ router.delete('/:postId', async (req,res) => {
 });
 
 // UPDATE POST
-router.patch('/:postId', async (req,res) => {
+router.patch('/:postId', verify, async (req,res) => {
     try{
         const updatedPost = await Post.updateOne(
             {_id: req.params.postId},
